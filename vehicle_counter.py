dbf6e75f4b20251341142b195e9399f6011bc8bd
Dicky Sanjaya <dicky0201@gmail.com>
2020-04-08 12:57:09 +0700

    init

---
+++ vehicle_counter.py
@@ -0,0 +1,48 @@
+import cv2
+import imutils
+import numpy as np
+
+
+cap = cv2.VideoCapture('media/highway.mp4')
+fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
+kernel_open = np.ones((7, 7), np.uint8)
+kernel_close = np.ones((15, 15), np.uint8)
+
+while(cap.isOpened()):
+    _, frame = cap.read()
+    fgmask = fgbg.apply(frame)
+
+    try:
+        # Apply binary threshold
+        _, binFrame = cv2.threshold(fgmask, 200, 255, cv2.THRESH_BINARY)
+        # Apply opening morph
+        opening = cv2.morphologyEx(binFrame, cv2.MORPH_OPEN, kernel_open)
+        # Apply closing morph
+        closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel_close)
+
+        cv2.imshow('Fg Mask', closing)
+    except:
+        print('EOF')
+        break
+
+    contours, hierarchy = cv2.findContours(
+        closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for contour in contours:
+        cv2.drawContours(frame, contour, 1, (0, 0, 255), 3, 8)
+        area = cv2.contourArea(contour)
+        print(area)
+        if area > 300:
+            m = cv2.moments(contour)
+            cx = int(m['m10']/m['m00'])
+            cy = int(m['m01']/m['m00'])
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
+            img = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('Frame', frame)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
