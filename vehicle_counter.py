51294e0671952715bced4a3527d24e2ea0ac379e
Dicky Sanjaya <dicky0201@gmail.com>
2020-04-14 12:19:09 +0700

    add comment

--- vehicle_counter.py
+++ vehicle_counter.py
@@ -1,16 +1,22 @@
 import cv2
 import numpy as np
 
+# pilih sumber video
 cap = cv2.VideoCapture('media/beachfront.mp4')
 
+# loop selama masih ada frame yang bisa dibaca
 while(cap.isOpened()):
+    # baca video per frame
     ret, frame = cap.read()
 
+    # tampilkan video perframe
     cv2.imshow("frame", frame)
 
+    # untuk keluar menggunakan esc
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
 
+# tutup dan hapus semua window yang terbuka
 cap.release()
 cv2.destroyAllWindows()
