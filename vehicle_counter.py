4c3651993f92b756ac3c434ea2879d3fe34c05bb
Dicky Sanjaya <dicky0201@gmail.com>
2020-04-14 12:26:30 +0700

    add bgmask

--- vehicle_counter.py
+++ vehicle_counter.py
@@ -3,14 +3,19 @@ import numpy as np
 
 # pilih sumber video
 cap = cv2.VideoCapture('media/beachfront.mp4')
+bgsub = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
+kernel = np.ones((5, 5), np.uint8)
 
 # loop selama masih ada frame yang bisa dibaca
 while(cap.isOpened()):
     # baca video per frame
     ret, frame = cap.read()
+    # apply bgsub
+    fgmask = bgsub.apply(frame)
 
     # tampilkan video perframe
     cv2.imshow("frame", frame)
+    cv2.imshow("mask", fgmask)
 
     # untuk keluar menggunakan esc
     k = cv2.waitKey(30) & 0xff
